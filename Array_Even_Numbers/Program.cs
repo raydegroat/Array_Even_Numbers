27a2a499d0a7f0b39b0ff2790e233d600d5a6576
raydegroat <stockholmray@gmail.com>
2020-05-13 18:14:39 +0200

    added comments

--- Array_Even_Numbers/Program.cs
+++ Array_Even_Numbers/Program.cs
@@ -4,12 +4,12 @@ namespace Array_Even_Numbers
 {
     class Program
     {
-        static int getInt()
+        static int getInt()    // A method to get user input and convert it to a number
         {
             int result = 0;
             string answer = Console.ReadLine();
 
-            Int32.TryParse(answer, out result);
+            Int32.TryParse(answer, out result);  // Convert string to 32 bit int
 
             return result;
         }
@@ -19,15 +19,15 @@ namespace Array_Even_Numbers
 
             int[] a = new int[10]; // Create an array of int and initialize it
 
-            for (int i = 0; i < a.Length; i++)
+            for (int i = 0; i < a.Length; i++)  // Get 10 numbers from the user
             {
                 Console.Write("Enter integer " + (i + 1) + " of " + a.Length + ": ");
                 a[i] = getInt();
             }
 
-            for (int i = 0; i < a.Length; i++)
+            for (int i = 0; i < a.Length; i++)  
             {
-                if(a[i] % 2 == 0)
+                if(a[i] % 2 == 0)  // If the numebr is even, print it
                     Console.WriteLine(a[i]);
             }
 
