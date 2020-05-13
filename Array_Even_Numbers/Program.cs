7b1c53a4f9327c0e52391be707cf18340ab7493c
raydegroat <stockholmray@gmail.com>
2020-05-13 18:08:20 +0200

    first commit

---
+++ Array_Even_Numbers/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace Array_Even_Numbers
+{
+    class Program
+    {
+        static int getInt()
+        {
+            int result = 0;
+            string answer = Console.ReadLine();
+
+            Int32.TryParse(answer, out result);
+
+            return result;
+        }
+
+        static void Main(string[] args)
+        {
+
+            int[] a = new int[10]; // Create an array of int and initialize it
+
+            for (int i = 0; i < a.Length; i++)
+            {
+                Console.Write("Enter integer " + (i + 1) + " of " + a.Length + ": ");
+                a[i] = getInt();
+            }
+
+            for (int i = 0; i < a.Length; i++)
+            {
+                if(a[i] % 2 == 0)
+                    Console.WriteLine(a[i]);
+            }
+
+        }
+    }
+}
